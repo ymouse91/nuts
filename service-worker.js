8f330573682de0a365eb6607ad6617f12235ef3c
ymouse91 <35890980+ymouse91@users.noreply.github.com>
2025-12-29 14:48:16 +0200

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE = "squirrels-test-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./puzzles.json",
+  "./icon182.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k))))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((hit) => hit || fetch(e.request))
+  );
+});
